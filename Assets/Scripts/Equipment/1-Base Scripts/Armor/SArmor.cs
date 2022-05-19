8c67e4db699e46ce841b33abd107a05a86b4a591
theVideoGamester <42751623+theVideoGamester@users.noreply.github.com>
2022-05-19 12:17:20 -0600

    Added Default Armor
    It gets auto equipped anyway but I figure why not have it available in the inspector

--- Assets/Scripts/Equipment/1-Base Scripts/Armor/SArmor.cs
+++ Assets/Scripts/Equipment/1-Base Scripts/Armor/SArmor.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct SArmor 
 {
+    [SerializeField]
     public string armorName;
+    [SerializeField]
     public int ac;
 
     public SArmor(string _armorName, int _ac)
